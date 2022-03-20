40bd181a06c24ee1fdfc354bf659ff34acf7ad94
miriani0996 <badbirds09>
2022-03-20 15:03:00 +0400

    react git

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import './App.scss';
-import WeekDays from "./components/Button";
-import Counter from './components/Counter';
+import Test from "./components/Test";
+// import WeekDays from "./components/Button";
+// import Counter from './components/Counter';
 
 function App() {
-    const days = [
-        { id: 1, day: 'Monday' },
-        { id: 2, day: 'Tuesday' },
-        { id: 3, day: 'Wednesday' },
-        { id: 4, day: 'Thursday' },
-        { id: 5, day: 'Friday' },
-        { id: 6, day: 'Saturday' },
-        { id: 7, day: 'Sunday' },
-    ]
+    // const days = [
+    //     { id: 1, day: 'Monday' },
+    //     { id: 2, day: 'Tuesday' },
+    //     { id: 3, day: 'Wednesday' },
+    //     { id: 4, day: 'Thursday' },
+    //     { id: 5, day: 'Friday' },
+    //     { id: 6, day: 'Saturday' },
+    //     { id: 7, day: 'Sunday' },
+    // ]
 
   return (
     <div>
-        <WeekDays days={days}/>
-        <Counter/>
+      <Test sum={2+2}/>
+        {/*<WeekDays days={days}/>*/}
+        {/*<Counter/>*/}
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/components/Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Test = (props) => {
+    return (
+        <div>
+            <h1>{props.sum}</h1>
+        </div>
+    );
+};
+
+export default Test;
+
